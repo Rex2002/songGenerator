9246cd41cc88b29820c4b8b1fc0f76a8f0538db8
ArtInLines <linesinart@gmail.com>
2022-11-15 09:40:53 +0100

    Implemented preprocessing for TextAnalyzer

--- src/main/java/org/se/Text/Analysis/Analyzer.java
+++ src/main/java/org/se/Text/Analysis/Analyzer.java
@@ -18,7 +18,53 @@ public class Analyzer {
 	}
 
 	static ArrayList<ArrayList<String>> preprocess(String text) {
+		// I'm sure there must be a better way to do this
+		// Maybe there's something you could do with streams to make this more readable
+		// but Java certainly doesn't make it easy to do any of this shit ffs
+		// I hate Java so fucking much, I'd rather be using C at this point aaaaaaaaaaaarrrrrgggggghhhhhhhhh
+		String wordSplitter = "-_";
+		Boolean splitLastWord = false;
+		String sentenceEnds = ".!?";
+		String otherPunctuation = ",;:-";
 		ArrayList<ArrayList<String>> sentences = new ArrayList<ArrayList<String>>();
+		ArrayList<String> currentSentence = new ArrayList<String>();
+		char[] chars = text.toCharArray();
+		String currentWord = "";
+		for (int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			if (Character.isWhitespace(c)) {
+				if (currentWord != "" && !splitLastWord) {
+					currentSentence.add(currentWord);
+					currentWord = "";
+				}
+			}
+			else if (sentenceEnds.indexOf(c) != -1) {
+				if (currentWord != "") {
+					currentSentence.add(currentWord);
+					currentWord = "";
+				}
+				splitLastWord = false;
+				ArrayList<String> copiedSentence = new ArrayList<String>(currentSentence);
+				sentences.add(copiedSentence);
+				currentSentence.clear();
+			}
+			else if (currentWord != "" && wordSplitter.indexOf(c) != -1) {
+				splitLastWord = true;
+			}
+			else if (otherPunctuation.indexOf(c) != -1) {
+				if (currentWord != "") {
+					currentSentence.add(currentWord);
+					currentWord = "";
+				}
+				splitLastWord = false;
+			} else {
+				currentWord += c;
+				splitLastWord = false;
+			}
+		}
+
+		if (currentWord != "") currentSentence.add(currentWord);
+		if (!currentSentence.isEmpty()) sentences.add(currentSentence);
 		return sentences;
 	}
 
