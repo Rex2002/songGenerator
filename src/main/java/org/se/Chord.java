0cea7a45c81e88e685985e353cfd75c25ae3fcfd
malterichert <s211206@student.dhbw-mannheim.de>
2022-11-15 20:14:45 +0100

    adds Jackson object binding for yml

--- src/main/java/org/se/Chord.java
+++ src/main/java/org/se/Chord.java
@@ -1,20 +1,31 @@
 package org.se;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 public class Chord {
     private int baseNote;
     private int[] chordModifier;
-    public static final Map<String, int[]> chordModifiers = new HashMap<>();
+    public static HashMap<String, int[]> chordModifiers;
     static {
-        chordModifiers.put("maj",new int[]{0,4,7});
+        /*chordModifiers.put("maj",new int[]{0,4,7});
         chordModifiers.put("m",new int[]{0,3,7});
         chordModifiers.put("maj7",new int[]{0,4,7,11});
         chordModifiers.put("m7",new int[]{0,3,7,11});
         chordModifiers.put("dim",new int[]{0,3,6});
-        chordModifiers.put("add9",new int[]{0,4,7, 14});
+        chordModifiers.put("add9",new int[]{0,4,7, 14}); */
 
+        //below code is not tested, not sure whether relative path works that way
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            chordModifiers = mapper.readValue(new File("resources/chord_modifiers.yml"), HashMap.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Chord(int baseNote, String chordModifier){
