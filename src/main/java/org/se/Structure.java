97bb17369896c1c9254953ec66542f001d3431aa
malterichert <s211206@student.dhbw-mannheim.de>
2022-11-16 20:33:32 +0100

    Adds draft model classes for Structure and Part as well as Enums for Instrument and Genre

---
+++ src/main/java/org/se/Structure.java
@@ -0,0 +1,25 @@
+package org.se;
+
+import java.util.List;
+
+public class Structure {
+    private List<Part> parts;
+    private MusicalKey key;
+    private Genre genre;
+
+    Structure(MusicalKey key, Genre genre){
+        this.key = key;
+        this.genre = genre;
+        //fill parts-list with structure from template
+    }
+
+    public List<Part> getParts() {
+        return parts;
+    }
+    public MusicalKey getKey() {
+        return key;
+    }
+    public Genre getGenre() {
+        return genre;
+    }
+}
