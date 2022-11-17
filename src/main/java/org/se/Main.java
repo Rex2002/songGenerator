2be0b99d38342767f1ad2c760e0b6403530b6d1f
ArtInLines <linesinart@gmail.com>
2022-11-17 12:11:12 +0100

    added simple example

--- src/main/java/org/se/Main.java
+++ src/main/java/org/se/Main.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 import org.se.Text.Analysis.Analyzer;
+import org.se.Text.Analysis.TermCollection;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-		Analyzer.analyze(Path.of("C:\\Users\\Administrator\\Documents\\Studium\\Software Engineering\\SongGenerator\\test.txt"));
+
+		// Analyzer.analyze(Path.of("C:\\Users\\Administrator\\Documents\\Studium\\Software Engineering\\SongGenerator\\test.txt"));
+		TermCollection c = TermExample.getExample();
+		System.out.println(c.terms.size());
     }
 }
