13869f417fc33594c41892ec813811e9b8b06833
ArtInLines <linesinart@gmail.com>
2022-11-29 21:38:03 +0100

    Bugfix, moved file & testing

--- src/main/java/org/se/Main.java
+++ src/main/java/org/se/Main.java
@@ -1,7 +1,17 @@
 package org.se;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+	public static void main(String[] args) throws IOException {
+		// Testing
+
+		String text = FileReader.main("test.txt");
+		System.out.println(text);
+
+		System.out.println("\n\n\n---\n\n\n");
+
+		text = FileReader.main("test.pdf");
+		System.out.println(text);
+	}
 }
