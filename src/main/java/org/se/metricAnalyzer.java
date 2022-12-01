bcdf1944a5f6b1dd84885d03323aca9ef5974f51
JakobPK <117927107+JakobPK@users.noreply.github.com>
2022-12-01 09:32:11 +0100

    Update metricAnalyzer.java
    Bugfixing, still got a bunch of runtime errors

--- src/main/java/org/se/metricAnalyzer.java
+++ src/main/java/org/se/metricAnalyzer.java
@@ -1,7 +1,4 @@
-public class metricAnalyzer{
-  
- 
-  public int main(String content, String terms){
+public int metricsGet(String content, String terms){
     //Find Average length for sentences and hyphen in order to determine text speed
       int averageH = averageHyphen(terms);
       int averageS = averageSentence(content);
@@ -25,37 +22,39 @@ public class metricAnalyzer{
       160 bpm average short
       180 bpm short short
      */
-    if(int averageH =< 1 & int averageS=<9)
+    if(averageH <= 1 & averageS<=9)
       {return bpm = 180;}
-    if(int averageH =2 | int averageH=3 & int averageS=<9)
+    if(averageH ==2 | averageH==3 & averageS<=9)
       {return bpm = 160;}
-    if(int averageH =2 | int averageH=3 & int averageS=>9 && int averageS =<18)
+    if(averageH ==2 | averageH==3 & averageS>=9 & averageS <=18)
       {return bpm = 140;}
-    if(int averageH =2 | int averageH=3 & int averageS=>19)
+    if(averageH ==2 | averageH==3 & averageS>=19)
       {return bpm = 120;}
-    if(int averageH=>4 & int averageS=<9)
+    if(averageH>=4 & averageS<=9)
       {return bpm = 100;}
-    if(int averageH=>4 & int averageS=>9 && int averageS =<18)
+    if(averageH>=4 & averageS>=9 & averageS <=18)
       {return bpm = 80;}
-    if(int averageH=>4 & int averageS=>19)
+    if(averageH>=4 & averageS>=19)
       {return bpm = 60;}
 
   }
 
   public int averageHyphen(String terms){
-    int hyphenTotal = CountSyllabes(String terms);
-    int[] termAndSentences = countWords(String terms);
+    String term = terms;
+    int hyphenTotal = CountSyllabes(term);
+    int[] termAndSentences = countWords(term);
     int termAmount = termAndSentences[0];
-    int averageH = hyphenTotal\termAmount;
+    int averageH = hyphenTotal/termAmount;
     return averageH;
   }
   
 
   public int averageSentence(String content){
-    int countWords(content);
-    int wordsTotal = wordArray[0];
-    int cTcSTotal = wordArray[1];
-  int sentenceAverage = wordsTotal/sentenceParts.length;
+    String c = content;
+    int[] termAndSentences = countWords(c);
+    int wordsTotal = termAndSentences[0];
+    int cTcSTotal = termAndSentences[1];
+  int sentenceAverage = wordsTotal/cTcSTotal;
   return sentenceAverage;
 }
   
@@ -69,7 +68,7 @@ public class metricAnalyzer{
      * 3. Free space between punctiation and new word
      * 4. New sentence for .!? and new paragraph
      */
-    char[] killTheSentence = ['.', '!', '?'];
+    String[] killTheSentence = {".!?"};
     String[] sentenceParts = c.split(killTheSentence);
     int wordsTotal = 0;
     int sentenceAverage = 0;
@@ -90,8 +89,8 @@ public class metricAnalyzer{
 
         
         if(state==1){
-          int state=0;
-          int words++;
+          state=0;
+          words = words+1;
         }
 
       }
@@ -99,10 +98,9 @@ public class metricAnalyzer{
 
     }
       int ctCsTotal = sentenceParts.length;
-      int[2] wordArray;
+      int[] wordArray;
       wordArray[0] = wordsTotal;
       wordArray[1] = ctCsTotal;
       
-      return int[]wordArray;
+      return wordArray;
   }
-}
