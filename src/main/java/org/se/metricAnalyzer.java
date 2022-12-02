5c988e0a40f163bedaa581a41e93c1324269b132
JakobPK <117927107+JakobPK@users.noreply.github.com>
2022-12-02 09:06:49 +0100

    Update metricAnalyzer.java

--- src/main/java/org/se/metricAnalyzer.java
+++ src/main/java/org/se/metricAnalyzer.java
@@ -1,3 +1,4 @@
+public class metricAnalyzer{
 public static int metricsGet(String content, String terms){
     //Find Average length for sentences and hyphen in order to determine text speed
       int averageH = averageHyphen(terms);
@@ -57,42 +58,6 @@ return bpm;
   int sentenceAverage = wordsTotal/cTcSTotal;
   return sentenceAverage;
 }
+} 
   
   
-  public static int[] countWords(String content){
-      String c = content;
-
-    /*Rules for sentences if done properly ig? Only stuck to punctuation for now
-     * 1. Upper Case for every new sentence
-     * 2. No new sentence if last character before punctuation mark was a number
-     * 3. Free space between punctiation and new word
-     * 4. New sentence for .!? and new paragraph
-     */
-    String[] sentenceParts = c.split(".!?");
-    int ctCsTotal = sentenceParts.length;
-    int wordsTotal = 0;
-    int words = 0;
-    int sentenceAverage = 0;
-    
-    for(int i = 0; i<sentenceParts.length; i++){
-
-      String currentSentence = sentenceParts[i];
-        words = 0;
-      for(int j=0; j<currentSentence.length();j++){ 
-
-        int state = 0;
-        if(currentSentence.charAt(j) == ' ' | currentSentence.charAt(j) == '\n' |currentSentence.charAt(j) == '\t'){
-          state = 1;
-        }
-        if(state==1){
-          words = words+1;
-        }
-      }
-      wordsTotal = wordsTotal + words;
-
-    }
-
-      int[] wordArray = {wordsTotal, ctCsTotal};
-      
-      return wordArray;
-  }
