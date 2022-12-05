9a113cc797cbf8ee5a88bd27e342ab9768f34d8b
MarekFischer <51700265+MarekFischer@users.noreply.github.com>
2022-12-05 18:09:10 +0100

    settings implementiert + icon

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
         primaryStage.setTitle("SongGenerator");
+        primaryStage.getIcons().add(new Image("file:img/icon.png"));
         primaryStage.setScene(new Scene(root, 800, 450));
         primaryStage.setResizable(false);
         primaryStage.show();
