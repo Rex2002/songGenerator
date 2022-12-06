8fee37539252d05f2c9cf72bf74850612d7f0a39
ArtInLines <linesinart@gmail.com>
2022-12-06 12:53:11 +0100

    fixed metric-files

--- src/main/java/org/se/WordCounter.java
+++ src/main/java/org/se/WordCounter.java
@@ -1,32 +1,34 @@
-public class WordCounter{
-  public static int[] countWords(String content){
-      String c = content;
-      String[] sentenceParts = c.split(".!?");
-      int ctCsTotal = sentenceParts.length;
-      int wordsTotal = 0;
-      int words = 0;
-      int sentenceAverage = 0;
-    
-    for(int i = 0; i<sentenceParts.length; i++){
+package org.se;
 
-      String currentSentence = sentenceParts[i];
-        words = 0;
-      for(int j=0; j<currentSentence.length();j++){ 
+public class WordCounter {
+	public static int[] countWords(String content) {
+		String c = content;
+		String[] sentenceParts = c.split(".!?");
+		int ctCsTotal = sentenceParts.length;
+		int wordsTotal = 0;
+		int words = 0;
+		int sentenceAverage = 0;
 
-        int state = 0;
-        if(currentSentence.charAt(j) == ' ' | currentSentence.charAt(j) == '\n' |currentSentence.charAt(j) == '\t'){
-          state = 1;
-        }
-        if(state==1){
-          words = words+1;
-        }
-      }
-      wordsTotal = wordsTotal + words;
+		for (int i = 0; i < sentenceParts.length; i++) {
 
-    }
+			String currentSentence = sentenceParts[i];
+			words = 0;
+			for (int j = 0; j < currentSentence.length(); j++) {
 
-      int[] wordArray = {wordsTotal, ctCsTotal};
-      
-      return wordArray;
-  }
+				int state = 0;
+				if (currentSentence.charAt(j) == ' ' | currentSentence.charAt(j) == '\n' | currentSentence.charAt(j) == '\t') {
+					state = 1;
+				}
+				if (state == 1) {
+					words = words + 1;
+				}
+			}
+			wordsTotal = wordsTotal + words;
+
+		}
+
+		int[] wordArray = { wordsTotal, ctCsTotal };
+
+		return wordArray;
+	}
 }
--- src/main/java/org/se/metricAnalyzer.java
+++ src/main/java/org/se/metricAnalyzer.java
@@ -1,63 +1,70 @@
-public class metricAnalyzer{
-public static int metricsGet(String content, String terms){
-    //Find Average length for sentences and hyphen in order to determine text speed
-      int averageH = averageHyphen(terms);
-      int averageS = averageSentence(content);
-      int bpm = 0;
+package org.se;
 
-    /*Define bpm for several sentence/hyphen length combinations
-     *1-9 Words is short sentence
-      10-18 words is average long sentence
-      18+ words is long sentence
-    Hyphen
-      1 hyphen is short word
-      2/3 hyphen is averae word
-      4+ hyphen is long word
+public class metricAnalyzer {
+	public static int metricsGet(String content, String terms) {
+		// Find Average length for sentences and hyphen in order to determine text speed
+		int averageH = averageHyphen(terms);
+		int averageS = averageSentence(content);
+		int bpm = 0;
 
-    bpm (hyphen sentence)
-      60bpm long long
-      80bpm long average
-      100 bpm long short
-      120 bpm average long
-      140 bpm average average
-      160 bpm average short
-      180 bpm short short
-     */
-    if(averageH <= 1 & averageS<=9)
-      {bpm = 180;}
-    if(averageH ==2 | averageH==3 & averageS<=9)
-      {bpm = 160;}
-    if(averageH ==2 | averageH==3 & averageS>=9 & averageS <=18)
-      {bpm = 140;}
-    if(averageH ==2 | averageH==3 & averageS>=19)
-      {bpm = 120;}
-    if(averageH>=4 & averageS<=9)
-      {bpm = 100;}
-    if(averageH>=4 & averageS>=9 & averageS <=18)
-      {bpm = 80;}
-    if(averageH>=4 & averageS>=19)
-      {bpm = 60;}
-return bpm;
-  }
+		/*
+		 * Define bpm for several sentence/hyphen length combinations
+		 * 1-9 Words is short sentence
+		 * 10-18 words is average long sentence
+		 * 18+ words is long sentence
+		 * Hyphen
+		 * 1 hyphen is short word
+		 * 2/3 hyphen is averae word
+		 * 4+ hyphen is long word
+		 *
+		 * bpm (hyphen sentence)
+		 * 60bpm long long
+		 * 80bpm long average
+		 * 100 bpm long short
+		 * 120 bpm average long
+		 * 140 bpm average average
+		 * 160 bpm average short
+		 * 180 bpm short short
+		 */
+		if (averageH <= 1 & averageS <= 9) {
+			bpm = 180;
+		}
+		if (averageH == 2 | averageH == 3 & averageS <= 9) {
+			bpm = 160;
+		}
+		if (averageH == 2 | averageH == 3 & averageS >= 9 & averageS <= 18) {
+			bpm = 140;
+		}
+		if (averageH == 2 | averageH == 3 & averageS >= 19) {
+			bpm = 120;
+		}
+		if (averageH >= 4 & averageS <= 9) {
+			bpm = 100;
+		}
+		if (averageH >= 4 & averageS >= 9 & averageS <= 18) {
+			bpm = 80;
+		}
+		if (averageH >= 4 & averageS >= 19) {
+			bpm = 60;
+		}
+		return bpm;
+	}
 
-  public static int averageHyphen(String terms){
-    String term = terms;
-    int hyphenTotal = CountSyllabes(term);
-    int[] termAndSentences = countWords(term);
-    int termAmount = termAndSentences[0];
-    int averageH = hyphenTotal/termAmount;
-    return averageH;
-  }
-  
+	public static int averageHyphen(String terms) {
+		String term = terms;
+		int hyphenTotal = Hyphenizer.CountSyllabes(term);
+		int[] termAndSentences = WordCounter.countWords(term);
+		int termAmount = termAndSentences[0];
+		int averageH = hyphenTotal / termAmount;
+		return averageH;
+	}
 
-  public static int averageSentence(String content){
-    String c = content;
-    int[] termAndSentences = countWords(c);
-    int wordsTotal = termAndSentences[0];
-    int cTcSTotal = termAndSentences[1];
-  int sentenceAverage = wordsTotal/cTcSTotal;
-  return sentenceAverage;
+	public static int averageSentence(String content) {
+		String c = content;
+		int[] termAndSentences = WordCounter.countWords(c);
+		int wordsTotal = termAndSentences[0];
+		int cTcSTotal = termAndSentences[1];
+		int sentenceAverage = wordsTotal / cTcSTotal;
+		return sentenceAverage;
+	}
 }
-} 
-  
-  
