a1159d5448f93bfd71ba739c3e94b5595403eb98
JakobPK <117927107+JakobPK@users.noreply.github.com>
2022-12-06 16:59:57 +0100

    Update WordCounter.java

--- src/main/java/org/se/WordCounter.java
+++ src/main/java/org/se/WordCounter.java
@@ -1,17 +1,17 @@
 public class WordCounter{
   public static int[] countWords(String content){
       String c = content;
-      String[] sentenceParts = c.split(".!?");
-      int ctCsTotal = sentenceParts.length;
+      List<String> sentenceParts = splitSentence(c);
+      int ctCsTotal = sentenceParts.size();
       int wordsTotal = 0;
       int words = 0;
       int sentenceAverage = 0;
     
-    for(int i = 0; i<sentenceParts.length; i++){
+    for(int i = 0; i<sentenceParts.size(); i++){
 
-      String currentSentence = sentenceParts[i];
+      String currentSentence = sentenceParts.get(i);
         words = 0;
-      for(int j=0; j<currentSentence.length();j++){ 
+      for(int j=0; j<currentSentence.size();j++){ 
 
         int state = 0;
         if(currentSentence.charAt(j) == ' ' | currentSentence.charAt(j) == '\n' |currentSentence.charAt(j) == '\t'){
@@ -29,4 +29,17 @@ public class WordCounter{
       
       return wordArray;
   }
+  
+  public static List<String> splitSentence(String content){
+	List<String> sentences = new ArrayList<String>;
+	int sentenceStart = 0;
+	for(int i = 0; i<content.length();i++){
+		if(content[i]=='.'|| content[i]=='!'|| content[i]=='?'){
+			sentences.add(content.substring(sentenceStart,i));
+			sentenceStart=i+1;
+		}
+	}
+	return sentences;
+}
+  
 }
