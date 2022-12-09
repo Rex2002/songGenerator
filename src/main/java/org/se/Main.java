59dec1f17bcc3b281bd76a63d0c43cb5838834c6
ArtInLines <linesinart@gmail.com>
2022-12-09 17:55:53 +0100

    Updated Main

--- src/main/java/org/se/Main.java
+++ src/main/java/org/se/Main.java
@@ -18,32 +18,22 @@ import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Config.loadConfig();
+		String filepath = args.length > 0 ? args[0] : "test.txt";
+		Map<String, Object> settings = Map.of("genre", Genre.POP, "nsfw", false, "tempo", 120);
 		Dict dictionary = Dict.getDefault();
 
-		// these are test values that will eventually be passed by UI and TextAnalyzer
-		String filepath = args.length > 0 ? args[0] : "test.txt";
-		Map<String, Object> settings = Map.of("genre", Genre.BLUES, "nsfw", false, "tempo", 120);
-
+		Config.loadConfig();
+		Config.getStructures().get(0).setGenre(Genre.POP);
 
 		String content = FileReader.main(filepath);
 		TermCollection terms = Analyzer.analyze(content, dictionary);
 		int metrics = MetricAnalyzer.metricsGet(content, terms);
 		StructureGenerator.generateStructure(settings, Map.of("tempo", metrics), terms);
-
-
-		System.out.println("\n\n\n");
-		System.out.println("Songtext:");
-
-
-		System.out.println("Metrics:");
-		System.out.println(metrics);
 	}
 }
 
-
 /**
  * Map<String, Object> settings = Map.of("genre", Genre.POP, "nsfw", false, "tempo", 120);
- * 		Map<String, Integer> metrics = Map.of("tempo", 70);
- * 		StructureGenerator.generateStructure(settings, metrics);
+ * Map<String, Integer> metrics = Map.of("tempo", 70);
+ * StructureGenerator.generateStructure(settings, metrics);
  */
