067c9624678639a21345dcb2d44baa2ce0d1ffa2
ArtInLines <linesinart@gmail.com>
2022-12-09 18:08:59 +0100

    Removed unnecessary TextAnalysis-related files

--- src/main/java/org/se/text/analysis/Test.java
+++ src/main/java/org/se/text/analysis/Test.java
@@ -1,15 +1,13 @@
 package org.se.text.analysis;
 
 import java.io.IOException;
-import java.util.*;
 
 import org.se.text.analysis.dict.Dict;
-import org.se.text.analysis.model.*;
 
 public class Test {
 	public static void main(String[] args) throws IOException {
 		Dict dict = Dict.getDefault();
-		String content = FileReader.main("test3.txt");
+		String content = FileReader.main("test2.txt");
 		TermCollection terms = Analyzer.analyze(content, dict);
 		terms.flatIter(term -> System.out.println(term));
 
